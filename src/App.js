0fc83b79762cf713aac8deb96e498179f9ed8ea0
leo <leonardobcarv@outlook.com>
2022-04-06 20:28:23 -0300

    -

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import SearchAppBar from './components/header/Header'
 
 function App() {
   return (
     <div className="App">
-     
+     <SearchAppBar/>
     </div>
   );
 }
